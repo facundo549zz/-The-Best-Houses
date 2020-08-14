c4dc11b30aad69e573d1490b22235103eefa6bf6
PatricioMar27 <patrix27yop@gmail.com>
2020-08-13 22:04:48 -0300

    Rutas y css

--- routes/registro.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require('../controllers/registroController') //requiero el controlador
-
-router.get('/', controller.listar) //utilizo el metodo listar del controlador
-
-module.exports = router //exporto todas las rutas
