1ec83697bec1335b01f789603fbb10d70c0ed687
AlejandroGuaymas <ale_guaymas@hotmail.com>
2020-09-07 15:20:51 -0300

    error delete

--- routes/error.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controller = require('../controllers/errorController');
-
-
-router.get('/',controller.error)
-
-module.exports = router;
