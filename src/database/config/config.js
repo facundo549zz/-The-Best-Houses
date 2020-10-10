11425d6bf9783dbe4eef16f084ca4bf106ea6990
AlejandroGuaymas <ale_guaymas@hotmail.com>
2020-10-10 17:06:14 -0300

    inicia sequelize y crea modelos

---
+++ src/database/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+  "development": {
+    "username": "root", //o prueba
+    "password": null,
+    "database": "thebestbikes",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
