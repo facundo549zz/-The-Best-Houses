fe5cb13cef0d8816fbfc41cfefe3b976d173c1c6
AlejandroGuaymas <ale_guaymas@hotmail.com>
2020-11-12 09:12:04 -0300

    productos search

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -23,21 +23,23 @@ module.exports = {
        db.Product.findByPk(id,{ 
             include : [
                 {
-                    asociation : 'categoria'
+                    association : 'categoria'
                 },
             ]
         })
         .then(bicicleta =>{
-            db.product.findAll({
+            producto = bicicleta
+            db.Product.findAll({
                 where:{
-                    id_categoria: bicicleta.categoria.id 
+                    id_categoria: bicicleta
                 }
             })
         })
-        .then(bicicletas =>{  
-        res.render('productDetail', {
+
+            .then(bicicletas =>{  
+            res.render('productDetail', {
             title: "Detalle del Producto",
-            producto: bicicleta.categoria,
+            producto: producto,
             bicicletas: bicicletas,
             css:"productDetail.css",
         })
